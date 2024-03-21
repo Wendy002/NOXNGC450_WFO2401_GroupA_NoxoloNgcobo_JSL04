c4d234e0f21529956b4a5c27228e0c0c7200a2b7
Noxolo <wendyngcobo98@gmail.com>
2024-03-21 16:23:37 +0200

    Solve stock check

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ console.log(`The user preference is ${userPreference === undefined ? 'Regular' :
 // Use a ternary operator to print "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
 
 let cansLeft = 3; // Any number of cans
-console.log(``);
+console.log(cansLeft  >= 5 ? `We're stocked!` : 'Time to restock!');
 
 
 // #### Challenge 3: Workout Intensity
